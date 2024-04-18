46a1af8f17eb91b56e62fd239c75d1cb9fc6784f
Books <dimaste7@mail.ru>
2024-04-19 02:40:17 +0300

    input data and constructor kleene's algorithm

--- incoming data/InputData.cpp
+++ incoming data/InputData.cpp
@@ -1,22 +1,80 @@
 #include "InputData.h"
 
 void InputData::inputData() {
-    std::ifstream            input;
+    std::ifstream            input("input.txt");
+    std::string              data;
+    std::vector<std::string> rightInputData = {"type",
+                                               "states",
+                                               "alphabet",
+                                               "initial",
+                                               "accepting",
+                                               "transitions",
+                                               "non-deterministic",
+                                               "deterministic"};
+
     std::string              typeFSA;
     std::vector<std::string> statesLabel;
     std::vector<std::string> alphabetSymbols;
     std::vector<std::string> initialStatesLabel;
     std::vector<std::string> acceptingStatesLabel;
+    std::vector<std::string> transitions;
 
-
-    if (!input.is_open()) {
+    if (!input.is_open())
         ErrorHandling::inputFileError();
+
+    while (getline(input, data)) {
+        std::map<std::string, int> mappingData;
+
+        mappingData["type"]         = 0;
+        mappingData["states"]       = 1;
+        mappingData["alphabet"]     = 2;
+        mappingData["initial"]      = 3;
+        mappingData["accepting"]    = 4;
+        mappingData["transitions"]  = 5;
+
+        std::string inputWord = data.substr(0, data.find('='));
+        data.erase(0, data.find('=') + 1).erase(0, data.find('[') + 1).erase(data.find(']'), data.find(']'));
+        if (std::find(rightInputData.begin(), rightInputData.end(), inputWord) == rightInputData.end())
+            ErrorHandling::inputFileError();
+
+        switch (mappingData[inputWord]) {
+            case 0:
+                typeFSA = data;
+                if (std::find(rightInputData.begin(), rightInputData.end(), typeFSA) == rightInputData.end())
+                    ErrorHandling::inputFileError();
+                break;
+
+            case 1:
+                statesLabel = split(data, ',');
+                break;
+
+            case 2:
+                alphabetSymbols = split(data, ',');
+                break;
+
+            case 3:
+                if (data.empty())
+                    ErrorHandling::initialStateError();
+                initialStatesLabel = split(data, ',');
+                break;
+
+            case 4:
+                if (data.empty())
+                    ErrorHandling::acceptingStateError();
+                acceptingStatesLabel = split(data, ',');
+                break;
+
+            case 5:
+                transitions = split(data, ',');
+                break;
+        }
     }
-
-
-
 
     input.close();
 
     // calling kleene algorithm that links with FSA
 }
+
+
+
+
--- kleene's algorithm/KleeneAlgorithm.h
+++ kleene's algorithm/KleeneAlgorithm.h
@@ -1,12 +1,21 @@
 #include "../main/include.h"
+#include "../fsa/FSA.h"
 
 #ifndef FSAREGEXTRANSLATOR_KLEENEALGORITHM_H
 #define FSAREGEXTRANSLATOR_KLEENEALGORITHM_H
 
 
 class KleeneAlgorithm {
-public:
+    FSA* fsa;
     std::vector<std::vector<std::string>> initialRegEx;
+public:
+    KleeneAlgorithm(const std::string &typeFSA,
+                    std::vector<std::string> &states,
+                    std::vector<std::string> &alphabet,
+                    std::vector<std::string> &initialStates,
+                    std::vector<std::string> &acceptingStates,
+                    std::vector<std::string> &transitions);
+
 
 };
 
